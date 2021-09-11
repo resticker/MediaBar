7d102a6a34d7850fafcaf35fef8b2d50282c22b2
Teodoro Parks <88119815+resticker@users.noreply.github.com>
2021-09-11 17:56:14 -0500

    trying to get tab working

--- macos/Preferences/GeneralPreferencesTabPageViewController.m
+++ macos/Preferences/GeneralPreferencesTabPageViewController.m
@@ -6,6 +6,9 @@
 
 #import "PreferencesCheckbox.h"
 #import "PreferencesPopUpButton.h"
+#import <MASShortcut/Shortcut.h>
+
+static NSString *const kPreferenceGlobalShortcut = @"GlobalShortcut";
 
 @interface GeneralPreferencesTabPageViewController ()
 
@@ -19,6 +22,7 @@
 @property (weak) IBOutlet PreferencesCheckbox *enableErrorReportingCheckbox;
 @property (weak) IBOutlet PreferencesPopUpButton *iconPopUpButton;
 @property (weak) IBOutlet PreferencesPopUpButton *iconWhilePlayingPopUpButton;
+@property (nonatomic, weak) IBOutlet MASShortcutView *shortcutView;
 
 @property (weak) IBOutlet NSButton *launchAtLoginCheckbox;
 
@@ -45,6 +49,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.shortcutView.associatedUserDefaultsKey = kPreferenceGlobalShortcut;
+    
+    
+    // Most apps need default shortcut, delete these lines if this is not your case
+//    MASShortcut *firstLaunchShortcut = [MASShortcut shortcutWithKeyCode:kVK_F1 modifierFlags:NSEventModifierFlagCommand];
+//    NSData *firstLaunchShortcutData = [NSKeyedArchiver archivedDataWithRootObject:firstLaunchShortcut];
+    
+//    [userDefaults registerDefaults:@{
+//        MASHardcodedShortcutEnabledKey : @YES,
+//        MASCustomShortcutEnabledKey : @YES,
+//        MASCustomShortcutKey : firstLaunchShortcutData
+//        kPreferenceGlobalShortcut : firstLaunchShortcutData
+//    }];
+    
+    
     [self setupCheckbox:self.showArtistCheckbox userDefaultsKey:ShowArtistUserDefaultsKey];
     [self setupCheckbox:self.showTitleCheckbox userDefaultsKey:ShowTitleUserDefaultsKey];
     [self setupCheckbox:self.showAlbumCheckbox userDefaultsKey:ShowAlbumUserDefaultsKey];
@@ -58,6 +77,22 @@
     self.maximumWidthSlider.integerValue = maximumWidth;
     
     self.launchAtLoginCheckbox.state = DNLoginServiceKit.loginItemExists ? NSControlStateValueOn : NSControlStateValueOff;
+    
+    [[MASShortcutBinder sharedBinder]
+        bindShortcutWithDefaultsKey:kPreferenceGlobalShortcut
+        toAction:^{
+            [self playShortcutFeedback];
+        // Let me know if you find a better or a more convenient API.
+    }];
+}
+
+- (void)playShortcutFeedback
+{
+    [[NSSound soundNamed:@"Ping"] play];
+//    [self.feedbackTextField setStringValue:NSLocalizedString(@"Shortcut pressed!", @"Feedback that’s displayed when user presses the sample shortcut.")];
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        [self.feedbackTextField setStringValue:@""];
+//    });
 }
 
 #pragma mark - Actions
---
+++ macos/Preferences/ShortcutsTabViewController.m
@@ -0,0 +1,19 @@
+#import "ShortcutsTabViewController.h"
+
+@interface ShortcutsTabViewController ()
+
+@end
+
+@implementation ShortcutsTabViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.preferredContentSize = NSMakeSize(self.view.frame.size.width, self.view.frame.size.height);
+}
+
+- (IBAction)githubButtonAction:(NSButton *)sender {
+    [NSWorkspace.sharedWorkspace openURL:[NSURL URLWithString:@"https://github.com/dimitarnestorov/MusicBar"]];
+}
+
+@end
